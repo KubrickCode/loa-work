acf8ab79ae6af181240bd341d665fb5241308c48
KubrickCode <kubrickcode@gmail.com>
2024-11-18 12:27:26 +0000

    쿠버네티스 인프라 설정

--- src/frontend/src/pages/page.tsx
+++ src/frontend/src/pages/page.tsx
@@ -8,5 +8,5 @@ export const Page = () => {
   if (error) return <div>Error: {error.message}</div>;
   if (!data) return <div>No data</div>;
 
-  return <>{data.minimumWage.amount}</>;
+  return <>현 최저 시급: {data.minimumWage.amount}</>;
 };
