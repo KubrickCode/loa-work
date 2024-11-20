57e74c5c99bc81a76a716a3d5404c3914ea0c1d6
KubrickCode <kubrickcode@gmail.com>
2024-11-20 12:23:52 +0000

    env 패키지에 GetEnvFallBack 추가
    간혹 env 유무와 관계 없이 동작할 수 있는 메서드 추가

--- src/go/libs/env/env.go
+++ src/go/libs/env/env.go
@@ -5,6 +5,13 @@ import (
 	"os"
 )
 
+func GetEnvFallback(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func MustGetEnv(key string) string {
 	value, ok := os.LookupEnv(key)
 	if !ok {
