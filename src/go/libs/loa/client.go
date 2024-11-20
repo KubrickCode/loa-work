f840b943e63c0696643420c4b2638d98067e33f2
KubrickCode <kubrickcode@gmail.com>
2024-11-20 12:28:57 +0000

    로아 API 클라이언트 생성

---
+++ src/go/libs/loa/client.go
@@ -0,0 +1,48 @@
+package loa
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/KubrickCode/loa-life/src/go/libs/env"
+	"github.com/KubrickCode/loa-life/src/go/libs/httpclient"
+	"github.com/pkg/errors"
+)
+
+type Client struct {
+	baseURL string
+	client  *httpclient.Client
+	token   string
+}
+
+func NewClient() *Client {
+	baseURL := env.GetEnvFallback("LOA_API_BASE_URL", "https://developer-lostark.game.onstove.com")
+	token := env.MustGetEnv("LOA_API_TOKEN")
+
+	return &Client{
+		baseURL: baseURL,
+		client:  httpclient.NewClient(),
+		token:   token,
+	}
+}
+
+func (c *Client) NewRequest() *httpclient.RequestBuilder {
+	rb := httpclient.NewRequestBuilder(c.baseURL)
+	rb.AddHeader("Authorization", fmt.Sprintf("Bearer %s", c.token))
+	return rb
+}
+
+func (c *Client) Do(req *http.Request, out interface{}) error {
+	err := c.client.Do(req, out)
+	if err != nil {
+		return err
+	}
+
+	_, err = json.Marshal(out)
+	if err != nil {
+		return errors.Wrapf(err, "failed to marshal output to check ResponseBase")
+	}
+
+	return nil
+}
