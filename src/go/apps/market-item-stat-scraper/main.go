1f769cb602ec93a992fc6bb1eae741ee7410195c
KubrickCode <kubrickcode@gmail.com>
2024-11-23 08:01:48 +0000

    마켓 항목 통계 수집 서비스 기본 구조 설정

---
+++ src/go/apps/market-item-stat-scraper/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/KubrickCode/loa-life/src/go/apps/market-item-scraper/scraper"
+	"github.com/KubrickCode/loa-life/src/go/libs/loadb"
+	"github.com/KubrickCode/loa-life/src/go/libs/schedule"
+)
+
+func main() {
+	db, err := loadb.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scraper := scraper.NewScraper(db)
+
+	scheduler := schedule.NewScheduler()
+	scheduler.AddTask(schedule.NewTask("Market Item Stat Scraping", time.Minute, scraper.Start))
+
+	err = scheduler.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
