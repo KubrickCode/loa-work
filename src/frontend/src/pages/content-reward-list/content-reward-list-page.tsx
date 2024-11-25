4e253171189393eab0591ae24267ffbd310ceff8
KubrickCode <kubrickcode@gmail.com>
2024-11-25 17:43:57 +0000

    수익 페이지 로딩 스켈레톤 설정 및 No Data 표시

--- src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
+++ src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@apollo/client";
+import { Box } from "@chakra-ui/react";
 
+import { SkeletonText } from "~/chakra-components/ui/skeleton";
 import {
   ContentRewardListPageDocument,
   ContentRewardListPageQuery,
@@ -10,9 +12,14 @@ import { Column, DataTable } from "~/core/table";
 export const ContentRewardListPage = () => {
   const { data, error, loading } = useQuery(ContentRewardListPageDocument);
 
-  if (loading) return <>loading...</>;
+  if (loading)
+    return (
+      <Box p={4}>
+        <SkeletonText noOfLines={30} gap="4" p={4} />
+      </Box>
+    );
   if (error) return <>{error.message}</>;
-  if (!data) return null;
+  if (!data) throw new Error("NO DATA");
 
   return (
     <Page>
