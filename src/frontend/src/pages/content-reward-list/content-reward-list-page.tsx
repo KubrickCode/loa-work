078fa92d0fbc103f2347ebb3885b21af0b305914
KubrickCode <kubrickcode@gmail.com>
2024-11-25 10:25:54 +0000

    컨텐츠 보상 페이지 테이블 기반으로 변경

--- src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
+++ src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
@@ -1,5 +1,10 @@
 import { useQuery } from "@apollo/client";
-import { ContentRewardListPageDocument } from "~/core/graphql/generated";
+
+import {
+  ContentRewardListPageDocument,
+  ContentRewardListPageQuery,
+} from "~/core/graphql/generated";
+import { DataTable } from "~/core/table";
 
 export const ContentRewardListPage = () => {
   const { data, error, loading } = useQuery(ContentRewardListPageDocument);
@@ -8,7 +13,41 @@ export const ContentRewardListPage = () => {
   if (error) return <>{error.message}</>;
   if (!data) return null;
 
-  console.log(data);
+  return (
+    <DataTable
+      columns={[
+        {
+          header: "종류",
+          render({ data }) {
+            return <>{data.type}</>;
+          },
+        },
+        {
+          header: "레벨",
+          render({ data }) {
+            return <>{data.level}</>;
+          },
+        },
+        {
+          header: "이름",
+          render({ data }) {
+            return <ContentName {...data} />;
+          },
+        },
+      ]}
+      rows={data.contentList.map((content) => ({
+        data: content,
+      }))}
+    />
+  );
+};
 
-  return <>CRL PAGE</>;
+const ContentName = ({
+  gate,
+  isSeeMore,
+  name,
+}: ContentRewardListPageQuery["contentList"][number]) => {
+  return (
+    <>{`${name}${gate ? ` ${gate}관문` : ""}${isSeeMore ? " 더보기" : ""}`}</>
+  );
 };
