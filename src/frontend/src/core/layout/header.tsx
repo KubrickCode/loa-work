aab899f161d72a35a18ea5e492df018eaf493a1b
KubrickCode <kubrickcode@gmail.com>
2024-11-25 13:18:39 +0000

    헤더 아바타 추가

--- src/frontend/src/core/layout/header.tsx
+++ src/frontend/src/core/layout/header.tsx
@@ -1,12 +1,17 @@
 import { Flex, Heading } from "@chakra-ui/react";
 
+import { Avatar } from "~/chakra-components/ui/avatar";
+
 import { Navigation } from "../navigation";
 
 export const Header = () => {
   return (
-    <Flex as="header" alignItems="center" gap={4}>
-      <Heading>로생</Heading>
-      <Navigation />
+    <Flex as="header" justifyContent="space-between">
+      <Flex alignItems="center" gap={4}>
+        <Heading>로생</Heading>
+        <Navigation />
+      </Flex>
+      <Avatar size="md" />
     </Flex>
   );
 };
