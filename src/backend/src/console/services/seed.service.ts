ee02668626b11c6527f3d0242f881f9e994e7f29
KubrickCode <kubrickcode@gmail.com>
2024-11-25 06:29:24 +0000

    컨텐츠 시드 데이터 생성

--- src/backend/src/console/services/seed.service.ts
+++ src/backend/src/console/services/seed.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma';
-import { Prisma } from '@prisma/client';
+import { ContentType, Prisma } from '@prisma/client';
 
 @Injectable()
 export class SeedService {
@@ -9,6 +9,7 @@ export class SeedService {
   async all() {
     await this.auctionItemCategories();
     await this.auctionItems();
+    await this.contents();
     await this.extraItems();
     await this.minimumWage();
   }
@@ -54,6 +55,98 @@ export class SeedService {
 
     await this.prisma.auctionItem.createMany({
       data: [...damageGems, ...coolDownGems],
+    });
+  }
+
+  async contents() {
+    const guardianRaids = [
+      {
+        level: 1640,
+        name: '아게오로스',
+      },
+      {
+        level: 1680,
+        name: '스콜라키아',
+      },
+    ].map((content) => ({
+      ...content,
+      type: ContentType.GUARDIAN_RAID,
+    }));
+
+    const kurzanFronts = [
+      {
+        level: 1640,
+        name: '아비도스 1 작전',
+      },
+      {
+        level: 1660,
+        name: '아비도스 2 작전',
+      },
+      {
+        level: 1680,
+        name: '아비도스 3 작전',
+      },
+    ].map((content) => ({
+      ...content,
+      type: ContentType.KURZAN_FRONT,
+    }));
+
+    const cubes = [
+      {
+        level: 1640,
+        name: '제 1 해금',
+      },
+      {
+        level: 1680,
+        name: '제 2 해금',
+      },
+    ].map((content) => ({
+      ...content,
+      type: ContentType.CUBE,
+    }));
+
+    const epicRaids = [{ level: 1640, name: '폭풍의 지휘관, 베히모스' }].map(
+      (content) => ({
+        ...content,
+        type: ContentType.EPIC_RAID,
+      }),
+    );
+
+    const kazerosRaids = [
+      { level: 1640, name: '[하드]붉어진 백야의 나선 - 1관문' },
+      { level: 1640, name: '[하드]붉어진 백야의 나선 - 2관문' },
+      { level: 1660, name: '[노말]대지를 부수는 업화의 궤적 - 1관문' },
+      { level: 1660, name: '[노말]대지를 부수는 업화의 궤적 - 2관문' },
+      { level: 1670, name: '[노말]부유하는 악몽의 진혼곡 - 1관문' },
+      { level: 1670, name: '[노말]부유하는 악몽의 진혼곡 - 2관문' },
+      { level: 1680, name: '[하드]대지를 부수는 업화의 궤적 - 1관문' },
+      { level: 1680, name: '[하드]대지를 부수는 업화의 궤적 - 2관문' },
+      { level: 1690, name: '[하드]부유하는 악몽의 진혼곡 - 1관문' },
+      { level: 1690, name: '[하드]부유하는 악몽의 진혼곡 - 2관문' },
+    ].map((content) => ({
+      ...content,
+      type: ContentType.KAZEROS_RAID,
+    }));
+
+    const legionCommanderRaids = [
+      { level: 1630, name: '[하드]어둠의 바라트론 - 1관문' },
+      { level: 1630, name: '[하드]어둠의 바라트론 - 2관문' },
+      { level: 1630, name: '[하드]어둠의 바라트론 - 3관문' },
+      { level: 1630, name: '[하드]어둠의 바라트론 - 4관문' },
+    ].map((content) => ({
+      ...content,
+      type: ContentType.LEGION_COMMANDER_RAID,
+    }));
+
+    await this.prisma.content.createMany({
+      data: [
+        ...guardianRaids,
+        ...kurzanFronts,
+        ...cubes,
+        ...epicRaids,
+        ...kazerosRaids,
+        ...legionCommanderRaids,
+      ],
     });
   }
 
