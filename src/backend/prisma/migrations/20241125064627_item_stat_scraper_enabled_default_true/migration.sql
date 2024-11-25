e1baaa36dd765066f24a68fb13eab219218f42dc
KubrickCode <kubrickcode@gmail.com>
2024-11-25 06:46:37 +0000

    아이템 수집 여부 default true 로 변경

---
+++ src/backend/prisma/migrations/20241125064627_item_stat_scraper_enabled_default_true/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "auction_item" ALTER COLUMN "is_stat_scraper_enabled" SET DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "market_item" ALTER COLUMN "is_stat_scraper_enabled" SET DEFAULT true;
