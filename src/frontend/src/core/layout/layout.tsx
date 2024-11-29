25951a7ca0f2be53c41fdd2c23ad886a7aed1314
KubrickCode <kubrickcode@gmail.com>
2024-11-29 12:49:21 +0000

    메인 컴포넌트 패딩 추가

--- src/frontend/src/core/layout/layout.tsx
+++ src/frontend/src/core/layout/layout.tsx
@@ -7,7 +7,9 @@ export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <Flex direction="column" px={8} py={4}>
       <Header />
-      <Box as="main">{children}</Box>
+      <Box as="main" p={3}>
+        {children}
+      </Box>
     </Flex>
   );
 };
