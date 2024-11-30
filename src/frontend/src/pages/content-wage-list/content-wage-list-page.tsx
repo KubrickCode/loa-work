d69438b553e73b2e6878d5dbe56db2bf4c1aed4f
KubrickCode <kubrickcode@gmail.com>
2024-11-30 14:30:10 +0000

    시급 페이지에서 전체 컨텐츠 테이블만 보여주도록 수정

--- src/frontend/src/pages/content-wage-list/content-wage-list-page.tsx
+++ src/frontend/src/pages/content-wage-list/content-wage-list-page.tsx
@@ -1,44 +1,12 @@
 import { Page } from "~/core/page";
-import { AccordionCard } from "~/core/accordion";
 import { ContentWageListTable } from "./components/content-wage-list-table";
-import { ContentType } from "~/core/graphql/generated";
-import { SplitLayout } from "~/core/layout";
 import { Section } from "~/core/section";
 
 export const ContentWageListPage = () => {
   return (
     <Page>
       <Section>
-        <SplitLayout
-          firstGroup={
-            <>
-              <AccordionCard title="전체 컨텐츠">
-                <ContentWageListTable />
-              </AccordionCard>
-              <AccordionCard title="쿠르잔 전선">
-                <ContentWageListTable contentType={ContentType.KURZAN_FRONT} />
-              </AccordionCard>
-              <AccordionCard title="가디언 토벌">
-                <ContentWageListTable contentType={ContentType.GUARDIAN_RAID} />
-              </AccordionCard>
-            </>
-          }
-          secondGroup={
-            <>
-              <AccordionCard title="카제로스 레이드">
-                <ContentWageListTable contentType={ContentType.KAZEROS_RAID} />
-              </AccordionCard>
-              <AccordionCard title="군단장 레이드">
-                <ContentWageListTable
-                  contentType={ContentType.LEGION_COMMANDER_RAID}
-                />
-              </AccordionCard>
-              <AccordionCard title="큐브">
-                <ContentWageListTable contentType={ContentType.CUBE} />
-              </AccordionCard>
-            </>
-          }
-        />
+        <ContentWageListTable />
       </Section>
     </Page>
   );
