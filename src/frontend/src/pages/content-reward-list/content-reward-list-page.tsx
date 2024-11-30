e8da0ca3271e8743a4043424ccf4f9e1248ddcb0
KubrickCode <kubrickcode@gmail.com>
2024-11-30 13:24:25 +0000

    컨텐츠별 수익 페이지에서 아코디언 카드 제거

--- src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
+++ src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
@@ -1,14 +1,11 @@
 import { Page } from "~/core/page";
 
 import { ContentRewardListTable } from "./components/content-reward-list-table";
-import { AccordionCard } from "~/core/accordion";
 
 export const ContentRewardListPage = () => {
   return (
     <Page>
-      <AccordionCard title="전체 컨텐츠">
-        <ContentRewardListTable />
-      </AccordionCard>
+      <ContentRewardListTable />
     </Page>
   );
 };
