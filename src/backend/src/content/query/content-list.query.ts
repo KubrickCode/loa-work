d2bb7555dd8b099e5ad90cdd568836994b140417
KubrickCode <kubrickcode@gmail.com>
2024-11-30 12:53:53 +0000

    컨텐츠 리스트 쿼리에 타입 필터 추가

--- src/backend/src/content/query/content-list.query.ts
+++ src/backend/src/content/query/content-list.query.ts
@@ -1,14 +1,34 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
+import { Args, Field, InputType, Query, Resolver } from '@nestjs/graphql';
 import { PrismaService } from 'src/prisma';
 import { Content } from '../object/content.object';
+import { ContentType, Prisma } from '@prisma/client';
+
+@InputType()
+export class ContentListFilter {
+  @Field(() => ContentType, { nullable: true })
+  contentType?: ContentType;
+}
 
 @Resolver()
 export class ContentListQuery {
   constructor(private prisma: PrismaService) {}
 
   @Query(() => [Content])
-  async contentList() {
-    return await this.prisma.content.findMany();
+  async contentList(
+    @Args('filter', { nullable: true }) filter?: ContentListFilter,
+  ) {
+    return await this.prisma.content.findMany({
+      where: this.buildWhereArgs(filter),
+    });
+  }
+
+  buildWhereArgs(filter?: ContentListFilter) {
+    const where: Prisma.ContentWhereInput = {};
+
+    if (filter?.contentType) {
+      where.type = filter.contentType;
+    }
+
+    return where;
   }
 }
