873b80a6ae2b6a75568baaf386932c331232773e
KubrickCode <kubrickcode@gmail.com>
2024-11-30 13:19:25 +0000

    컨텐츠별 수익 페이지 테이블 컴포넌트 분리

---
+++ src/frontend/src/pages/content-reward-list/components/content-reward-list-table.tsx
@@ -0,0 +1,69 @@
+import { useQuery } from "@apollo/client";
+import { Box } from "@chakra-ui/react";
+import { SkeletonText } from "~/chakra-components/ui/skeleton";
+import {
+  ContentRewardListTableDocument,
+  ContentRewardListTableQuery,
+} from "~/core/graphql/generated";
+import { Column, DataTable } from "~/core/table";
+
+export const ContentRewardListTable = () => {
+  const { data, error, loading } = useQuery(ContentRewardListTableDocument);
+
+  if (loading)
+    return (
+      <Box p={4}>
+        <SkeletonText noOfLines={30} gap="4" p={4} />
+      </Box>
+    );
+  if (error) return <>{error.message}</>;
+  if (!data) throw new Error("NO DATA");
+
+  return (
+    <DataTable
+      columns={[
+        {
+          header: "종류",
+          render({ data }) {
+            return <>{data.displayTypeName}</>;
+          },
+        },
+        {
+          header: "레벨",
+          render({ data }) {
+            return <>{data.level}</>;
+          },
+        },
+        {
+          header: "이름",
+          render({ data }) {
+            return <>{data.displayName}</>;
+          },
+        },
+        ...data.contentRewardViewList.map(
+          (
+            itemName
+          ): Column<ContentRewardListTableQuery["contentList"][number]> => ({
+            align: "right",
+            header: itemName,
+            render({ data }) {
+              const reward = data.contentRewards.find(
+                (reward) => reward.itemName === itemName
+              );
+              return (
+                <>
+                  {reward
+                    ? `${reward.averageQuantity}${reward.isSellable && reward.itemName !== "골드" ? " (거래 가능)" : ""}`
+                    : "-"}
+                </>
+              );
+            },
+          })
+        ),
+      ]}
+      rows={data.contentList.map((content) => ({
+        data: content,
+      }))}
+    />
+  );
+};
--- src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
+++ src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
@@ -1,73 +1,11 @@
-import { useQuery } from "@apollo/client";
-import { Box } from "@chakra-ui/react";
-
-import { SkeletonText } from "~/chakra-components/ui/skeleton";
-import {
-  ContentRewardListPageDocument,
-  ContentRewardListPageQuery,
-} from "~/core/graphql/generated";
 import { Page } from "~/core/page";
-import { Column, DataTable } from "~/core/table";
+
+import { ContentRewardListTable } from "./components/content-reward-list-table";
 
 export const ContentRewardListPage = () => {
-  const { data, error, loading } = useQuery(ContentRewardListPageDocument);
-
-  if (loading)
-    return (
-      <Box p={4}>
-        <SkeletonText noOfLines={30} gap="4" p={4} />
-      </Box>
-    );
-  if (error) return <>{error.message}</>;
-  if (!data) throw new Error("NO DATA");
-
   return (
     <Page>
-      <DataTable
-        columns={[
-          {
-            header: "종류",
-            render({ data }) {
-              return <>{data.displayTypeName}</>;
-            },
-          },
-          {
-            header: "레벨",
-            render({ data }) {
-              return <>{data.level}</>;
-            },
-          },
-          {
-            header: "이름",
-            render({ data }) {
-              return <>{data.displayName}</>;
-            },
-          },
-          ...data.contentRewardViewList.map(
-            (
-              itemName
-            ): Column<ContentRewardListPageQuery["contentList"][number]> => ({
-              align: "right",
-              header: itemName,
-              render({ data }) {
-                const reward = data.contentRewards.find(
-                  (reward) => reward.itemName === itemName
-                );
-                return (
-                  <>
-                    {reward
-                      ? `${reward.averageQuantity}${reward.isSellable && reward.itemName !== "골드" ? " (거래 가능)" : ""}`
-                      : "-"}
-                  </>
-                );
-              },
-            })
-          ),
-        ]}
-        rows={data.contentList.map((content) => ({
-          data: content,
-        }))}
-      />
+      <ContentRewardListTable />
     </Page>
   );
 };
