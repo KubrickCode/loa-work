914dbd25b99f499ca06abeac34c580a7a66bff58
KubrickCode <kubrickcode@gmail.com>
2024-11-30 13:20:03 +0000

    컨텐츠별 수익 테이블 아코디언 카드로 변경

--- src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
+++ src/frontend/src/pages/content-reward-list/content-reward-list-page.tsx
@@ -1,11 +1,14 @@
 import { Page } from "~/core/page";
 
 import { ContentRewardListTable } from "./components/content-reward-list-table";
+import { AccordionCard } from "~/core/accordion";
 
 export const ContentRewardListPage = () => {
   return (
     <Page>
-      <ContentRewardListTable />
+      <AccordionCard title="전체 컨텐츠">
+        <ContentRewardListTable />
+      </AccordionCard>
     </Page>
   );
 };
