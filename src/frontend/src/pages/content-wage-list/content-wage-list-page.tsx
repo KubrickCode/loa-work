ee10f08bc9245777a3c24823e8003b064c9d3f89
KubrickCode <kubrickcode@gmail.com>
2024-11-30 13:05:32 +0000

    시급 페이지에 각 컨텐츠별로 카드 분리

--- src/frontend/src/pages/content-wage-list/content-wage-list-page.tsx
+++ src/frontend/src/pages/content-wage-list/content-wage-list-page.tsx
@@ -2,6 +2,7 @@ import { Flex, VStack } from "@chakra-ui/react";
 import { Page } from "~/core/page";
 import { AccordionCard } from "~/core/accordion";
 import { ContentWageListTable } from "./components/content-wage-list-table";
+import { ContentType } from "~/core/graphql/generated";
 
 export const ContentWageListPage = () => {
   return (
@@ -11,11 +12,20 @@ export const ContentWageListPage = () => {
           <AccordionCard title="전체 컨텐츠">
             <ContentWageListTable />
           </AccordionCard>
-          <AccordionCard title="Empty State">Empty State</AccordionCard>
+          <AccordionCard title="가디언 토벌">
+            <ContentWageListTable contentType={ContentType.GUARDIAN_RAID} />
+          </AccordionCard>
+          <AccordionCard title="카제로스 레이드">
+            <ContentWageListTable contentType={ContentType.KAZEROS_RAID} />
+          </AccordionCard>
         </VStack>
         <VStack flex="1" gap={4}>
-          <AccordionCard title="Empty State">Empty State</AccordionCard>
-          <AccordionCard title="Empty State">Empty State</AccordionCard>
+          <AccordionCard title="쿠르잔 전선">
+            <ContentWageListTable contentType={ContentType.KURZAN_FRONT} />
+          </AccordionCard>
+          <AccordionCard title="큐브">
+            <ContentWageListTable contentType={ContentType.CUBE} />
+          </AccordionCard>
         </VStack>
       </Flex>
     </Page>
