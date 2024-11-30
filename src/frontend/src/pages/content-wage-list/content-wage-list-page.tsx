61419c931bc268ce2e2a59a946fa27c3331ba5b7
KubrickCode <kubrickcode@gmail.com>
2024-11-30 13:15:52 +0000

    시급별 컨텐츠 중요도 순으로 보드 순서 변경

--- src/frontend/src/pages/content-wage-list/content-wage-list-page.tsx
+++ src/frontend/src/pages/content-wage-list/content-wage-list-page.tsx
@@ -13,26 +13,26 @@ export const ContentWageListPage = () => {
             <AccordionCard title="전체 컨텐츠">
               <ContentWageListTable />
             </AccordionCard>
+            <AccordionCard title="쿠르잔 전선">
+              <ContentWageListTable contentType={ContentType.KURZAN_FRONT} />
+            </AccordionCard>
             <AccordionCard title="가디언 토벌">
               <ContentWageListTable contentType={ContentType.GUARDIAN_RAID} />
-            </AccordionCard>
-            <AccordionCard title="카제로스 레이드">
-              <ContentWageListTable contentType={ContentType.KAZEROS_RAID} />
             </AccordionCard>
           </>
         }
         secondGroup={
           <>
-            <AccordionCard title="쿠르잔 전선">
-              <ContentWageListTable contentType={ContentType.KURZAN_FRONT} />
-            </AccordionCard>
-            <AccordionCard title="큐브">
-              <ContentWageListTable contentType={ContentType.CUBE} />
+            <AccordionCard title="카제로스 레이드">
+              <ContentWageListTable contentType={ContentType.KAZEROS_RAID} />
             </AccordionCard>
             <AccordionCard title="군단장 레이드">
               <ContentWageListTable
                 contentType={ContentType.LEGION_COMMANDER_RAID}
               />
+            </AccordionCard>
+            <AccordionCard title="큐브">
+              <ContentWageListTable contentType={ContentType.CUBE} />
             </AccordionCard>
           </>
         }
