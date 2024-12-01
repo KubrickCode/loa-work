345e191f60040f72d5468c4e31a8b58ff457ebc4
KubrickCode <kubrickcode@gmail.com>
2024-12-01 13:33:23 +0000

    메뉴 트리거 스타일 변경

--- src/frontend/src/core/layout/header/header-menu.tsx
+++ src/frontend/src/core/layout/header/header-menu.tsx
@@ -1,5 +1,7 @@
+import { IoIosArrowDown } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 import { Avatar } from "~/chakra-components/ui/avatar";
+import { Button } from "~/chakra-components/ui/button";
 import { ColorModeIcon, useColorMode } from "~/chakra-components/ui/color-mode";
 import {
   MenuContent,
@@ -17,7 +19,11 @@ export const HeaderMenu = () => {
   return (
     <MenuRoot closeOnSelect={false} positioning={{ placement: "bottom" }}>
       <MenuTrigger>
-        <Avatar cursor="pointer" size="md" src={user?.imageUrl} />
+        <Button variant="ghost" px={2}>
+          <Avatar size="xs" src={user?.imageUrl} />
+          {user?.displayName}
+          <IoIosArrowDown />
+        </Button>
       </MenuTrigger>
       <MenuContent>
         <MenuItem onClick={toggleColorMode} value="dark-mode">
