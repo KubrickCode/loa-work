817062ef7f76a4a18af0669b55c1147573301244
KubrickCode <kubrickcode@gmail.com>
2024-12-01 07:18:57 +0000

    아이템 시세 페이지 SplitLayout 적용

--- src/frontend/src/pages/item-price-list/item-price-list-page.tsx
+++ src/frontend/src/pages/item-price-list/item-price-list-page.tsx
@@ -1,13 +1,25 @@
 import { Page } from "~/core/page";
 import { Section } from "~/core/section";
+import { SplitLayout } from "~/core/layout";
+
 import { MarketItemListTable } from "./components/market-item-list-table";
+import { AuctionItemListTable } from "./components/auction-item-list-table";
 
 export const ItemPriceListPage = () => {
   return (
     <Page>
-      <Section>
-        <MarketItemListTable />
-      </Section>
+      <SplitLayout
+        firstGroup={
+          <Section>
+            <MarketItemListTable />
+          </Section>
+        }
+        secondGroup={
+          <Section>
+            <AuctionItemListTable />
+          </Section>
+        }
+      />
     </Page>
   );
 };
