d69001821a975885b701ed4910f84effe2d6b6cc
KubrickCode <kubrickcode@gmail.com>
2024-12-01 06:03:59 +0000

    스크랩 여부 기본 값 false로 설정

---
+++ src/backend/prisma/migrations/20241201060344_scraper_enabled_default_false/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "auction_item" ALTER COLUMN "is_stat_scraper_enabled" SET DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "market_item" ALTER COLUMN "is_stat_scraper_enabled" SET DEFAULT false;
