af31b3e4094213450aca81d746bf80b4027c6d87
KubrickCode <kubrickcode@gmail.com>
2024-12-01 03:40:40 +0000

    wage 골드 계산 메서드 분리

--- src/backend/src/content/object/content.resolver.ts
+++ src/backend/src/content/object/content.resolver.ts
@@ -5,6 +5,7 @@ import { ContentReward } from './content-reward.object';
 import { ContentRewardKind } from 'src/enums';
 import { ItemPriceService } from '../service/item-price.service';
 import { ContentCategory } from './content-category.object';
+import * as Prisma from '@prisma/client';
 
 @Resolver(() => Content)
 export class ContentResolver {
@@ -48,6 +49,43 @@ export class ContentResolver {
       },
     });
 
+    let gold = await this.calculateGold(rewards);
+
+    if (filter?.includeIsSeeMore === true) {
+      const seeMoreContents = await this.prisma.content.findMany({
+        where: {
+          name: content.name,
+          gate: content.gate,
+          isSeeMore: true,
+        },
+      });
+
+      for (const seeMoreContent of seeMoreContents) {
+        const seeMoreRewards = await this.prisma.contentReward.findMany({
+          where: {
+            contentId: seeMoreContent.id,
+          },
+        });
+
+        gold += await this.calculateGold(seeMoreRewards);
+      }
+    }
+
+    const goldExchangeRate =
+      await this.prisma.goldExchangeRate.findFirstOrThrow({
+        take: 1,
+      });
+
+    const totalKRW =
+      (gold * goldExchangeRate.goldAmount) / goldExchangeRate.krwAmount;
+
+    const hours = content.duration / 3600;
+    const hourlyWage = totalKRW / hours;
+
+    return Math.round(hourlyWage);
+  }
+
+  private async calculateGold(rewards: Prisma.ContentReward[]) {
     let gold = 0;
 
     for (const reward of rewards) {
@@ -82,72 +120,7 @@ export class ContentResolver {
         default:
           throw new Error(`Unknown reward kind: ${reward.itemName}`);
       }
-
-      if (filter?.includeIsSeeMore) {
-        const seeMoreContents = await this.prisma.content.findMany({
-          where: {
-            name: content.name,
-            gate: content.gate,
-            isSeeMore: true,
-          },
-        });
-
-        for (const seeMoreContent of seeMoreContents) {
-          const seeMoreRewards = await this.prisma.contentReward.findMany({
-            where: {
-              contentId: seeMoreContent.id,
-            },
-          });
-
-          for (const reward of seeMoreRewards) {
-            const averageQuantity = reward.averageQuantity.toNumber();
-
-            switch (reward.itemName) {
-              case ContentRewardKind.GOLD:
-                gold += averageQuantity;
-                break;
-              case ContentRewardKind.LEVEL_1_GEM:
-                gold +=
-                  (await this.itemPriceService.get1LevelGemPrice()) *
-                  averageQuantity;
-                break;
-              case ContentRewardKind.FATE_FRAGMENT:
-                gold +=
-                  (await this.itemPriceService.getSmallFateFragmentBuyPricePerOne()) *
-                  averageQuantity;
-                break;
-              case ContentRewardKind.FATE_BREAKTHROUGH_STONE:
-              case ContentRewardKind.FATE_DESTRUCTION_STONE:
-              case ContentRewardKind.FATE_GUARDIAN_STONE:
-              case ContentRewardKind.LAVA_BREATH:
-              case ContentRewardKind.GLACIER_BREATH:
-                gold +=
-                  (await this.itemPriceService.getMarketItemCurrentMinPrice(
-                    reward.itemName,
-                  )) * averageQuantity;
-                break;
-              case ContentRewardKind.CARD_EXP:
-              case ContentRewardKind.SHILLING:
-                break;
-              default:
-                throw new Error(`Unknown reward kind: ${reward.itemName}`);
-            }
-          }
-        }
-      }
     }
-
-    const goldExchangeRate =
-      await this.prisma.goldExchangeRate.findFirstOrThrow({
-        take: 1,
-      });
-
-    const totalKRW =
-      (gold * goldExchangeRate.goldAmount) / goldExchangeRate.krwAmount;
-
-    const hours = content.duration / 3600;
-    const hourlyWage = totalKRW / hours;
-
-    return Math.round(hourlyWage);
+    return gold;
   }
 }
