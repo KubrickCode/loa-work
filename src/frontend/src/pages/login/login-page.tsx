68ac6a1733519777a1063ab74714d13b84583448
KubrickCode <kubrickcode@gmail.com>
2024-12-02 11:08:32 +0000

    프론트엔드 디스코드 로그인 버튼 추가

--- src/frontend/src/pages/login/login-page.tsx
+++ src/frontend/src/pages/login/login-page.tsx
@@ -11,6 +11,13 @@ export const LoginPage = () => {
       >
         구글 로그인
       </Button>
+      <Button
+        onClick={() => {
+          window.location.href = import.meta.env.VITE_DISCORD_LOGIN_URL;
+        }}
+      >
+        디스코드 로그인
+      </Button>
     </Page>
   );
 };
