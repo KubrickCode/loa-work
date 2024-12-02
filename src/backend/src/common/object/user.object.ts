450babe1a5c19f7f1e88f96ea7d51dc919a15f0f
KubrickCode <kubrickcode@gmail.com>
2024-12-02 13:05:32 +0000

    User Object 생성

---
+++ src/backend/src/common/object/user.object.ts
@@ -0,0 +1,24 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { BaseObject } from './base.object';
+import { AuthProvider, UserRole } from '@prisma/client';
+
+@ObjectType()
+export class User extends BaseObject {
+  @Field()
+  displayName: string;
+
+  @Field()
+  email: string;
+
+  @Field(() => String, { nullable: true })
+  imageUrl?: string;
+
+  @Field(() => AuthProvider)
+  provider: AuthProvider;
+
+  @Field()
+  refId: string;
+
+  @Field(() => UserRole)
+  role: UserRole;
+}
