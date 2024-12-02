bd780a51967328299acb63a0ce6472f6e3232d3f
KubrickCode <kubrickcode@gmail.com>
2024-12-02 11:43:59 +0000

    모바일 레이아웃 패딩 조정

--- src/frontend/src/core/layout/layout.tsx
+++ src/frontend/src/core/layout/layout.tsx
@@ -5,7 +5,7 @@ import { Header } from "./header";
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <Flex direction="column" px={8} py={4}>
+    <Flex direction="column" px={{ base: 0, md: 8 }} py={{ base: 0, md: 4 }}>
       <Header />
       <Box as="main" p={3}>
         {children}
