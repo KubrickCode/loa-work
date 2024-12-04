b195659d587e7adc1df6b81e81e3a8779e5f92f0
KubrickCode <kubrickcode@gmail.com>
2024-12-04 12:24:24 +0000

    모달 내 종료버튼 추가

--- src/frontend/src/core/dialog/dialog.tsx
+++ src/frontend/src/core/dialog/dialog.tsx
@@ -1,3 +1,12 @@
-import { DialogContent } from "~/chakra-components/ui/dialog";
+import { PropsWithChildren } from "react";
+import {
+  DialogCloseTrigger,
+  DialogContent,
+} from "~/chakra-components/ui/dialog";
 
-export const Dialog = DialogContent;
+export const Dialog = ({ children, ...props }: PropsWithChildren) => (
+  <DialogContent {...props}>
+    {children}
+    <DialogCloseTrigger />
+  </DialogContent>
+);
