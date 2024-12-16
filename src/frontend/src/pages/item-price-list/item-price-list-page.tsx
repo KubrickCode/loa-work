eee411093cefa2b365fad9a62ea11eeccf421d93
KubrickCode <kubrickcode@gmail.com>
2024-12-16 12:17:14 +0000

    기타 아이템과 재련 재료 아이템 섹션 위치 변경

--- src/frontend/src/pages/item-price-list/item-price-list-page.tsx
+++ src/frontend/src/pages/item-price-list/item-price-list-page.tsx
@@ -14,14 +14,14 @@ export const ItemPriceListPage = () => {
       <SplitLayout
         firstGroup={
           <>
-            <AccordionCard title="재련 재료">
-              <Suspense fallback={<Loader.TableSkeleton line={10} />}>
-                <MarketItemListTable categoryName="재련 재료" />
-              </Suspense>
-            </AccordionCard>
             <AccordionCard title="기타 아이템">
               <Suspense fallback={<Loader.TableSkeleton line={2} />}>
                 <ExtraItemListTable />
+              </Suspense>
+            </AccordionCard>
+            <AccordionCard title="재련 재료">
+              <Suspense fallback={<Loader.TableSkeleton line={10} />}>
+                <MarketItemListTable categoryName="재련 재료" />
               </Suspense>
             </AccordionCard>
           </>
