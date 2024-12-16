a8f66283d309bf7e35161741ca6190604b50ae72
KubrickCode <kubrickcode@gmail.com>
2024-12-16 12:44:01 +0000

    거래소 아이템 탭 SplitLayout 적용

--- src/frontend/src/pages/item-price-list/tabs/market-item-tab/market-item-tab.tsx
+++ src/frontend/src/pages/item-price-list/tabs/market-item-tab/market-item-tab.tsx
@@ -2,25 +2,32 @@ import { Suspense } from "react";
 import { AccordionCard } from "~/core/accordion";
 import { Loader } from "~/core/loader";
 import { MarketItemListTable } from "./components/market-item-list-table";
+import { SplitLayout } from "~/core/layout";
 
 export const MarketItemTab = () => {
   return (
-    <>
-      <AccordionCard title="재련 재료">
-        <Suspense fallback={<Loader.TableSkeleton line={10} />}>
-          <MarketItemListTable categoryName="재련 재료" />
-        </Suspense>
-      </AccordionCard>
-      <AccordionCard title="재련 추가 재료">
-        <Suspense fallback={<Loader.TableSkeleton line={10} />}>
-          <MarketItemListTable categoryName="재련 추가 재료" />
-        </Suspense>
-      </AccordionCard>
-      <AccordionCard title="유물 각인서">
-        <Suspense fallback={<Loader.TableSkeleton line={10} />}>
-          <MarketItemListTable categoryName="각인서" grade="유물" />
-        </Suspense>
-      </AccordionCard>
-    </>
+    <SplitLayout
+      firstGroup={
+        <>
+          <AccordionCard title="재련 재료">
+            <Suspense fallback={<Loader.TableSkeleton line={10} />}>
+              <MarketItemListTable categoryName="재련 재료" />
+            </Suspense>
+          </AccordionCard>
+          <AccordionCard title="재련 추가 재료">
+            <Suspense fallback={<Loader.TableSkeleton line={10} />}>
+              <MarketItemListTable categoryName="재련 추가 재료" />
+            </Suspense>
+          </AccordionCard>
+        </>
+      }
+      secondGroup={
+        <AccordionCard title="유물 각인서">
+          <Suspense fallback={<Loader.TableSkeleton line={10} />}>
+            <MarketItemListTable categoryName="각인서" grade="유물" />
+          </Suspense>
+        </AccordionCard>
+      }
+    />
   );
 };
