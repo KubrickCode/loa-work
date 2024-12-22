9a1b800ebb0469685bacfaa27945ea1cf4698e78
KubrickCode <kubrickcode@gmail.com>
2024-12-22 02:47:52 +0000

    디스코드, 카카오 관련 config assert 추가

--- src/backend/src/common/config.ts
+++ src/backend/src/common/config.ts
@@ -9,6 +9,14 @@ export default () => {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
+    discord: {
+      clientID: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    },
+    kakao: {
+      clientID: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    },
   };
 
   assert(
@@ -19,6 +27,26 @@ export default () => {
   assert(
     config.google.clientSecret,
     'GOOGLE_CLIENT_SECRET environment variable is missing',
+  );
+
+  assert(
+    config.discord.clientID,
+    'DISCORD_CLIENT_ID environment variable is missing',
+  );
+
+  assert(
+    config.discord.clientSecret,
+    'DISCORD_CLIENT_SECRET environment variable is missing',
+  );
+
+  assert(
+    config.kakao.clientID,
+    'KAKAO_CLIENT_ID environment variable is missing',
+  );
+
+  assert(
+    config.kakao.clientSecret,
+    'KAKAO_CLIENT_SECRET environment variable is missing',
   );
 
   return config;
