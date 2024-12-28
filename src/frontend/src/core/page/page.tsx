03aa325e5e05337fe76f1bf467b9ecde46bc41bc
KubrickCode <kubrickcode@gmail.com>
2024-12-28 06:18:13 +0000

    page 컴포넌트 기본 suspense 추가

--- src/frontend/src/core/page/page.tsx
+++ src/frontend/src/core/page/page.tsx
@@ -1,12 +1,16 @@
 import { StackProps, VStack } from "@chakra-ui/react";
 import { ErrorBoundary } from "../error";
+import { Suspense } from "react";
+import { Loader } from "../loader";
 
 export type PageProps = StackProps;
 
 export const Page = ({ children, ...otherProps }: PageProps) => {
   return (
     <VStack align="stretch" {...otherProps}>
-      <ErrorBoundary>{children}</ErrorBoundary>
+      <ErrorBoundary>
+        <Suspense fallback={<Loader.Page />}>{children}</Suspense>
+      </ErrorBoundary>
     </VStack>
   );
 };
