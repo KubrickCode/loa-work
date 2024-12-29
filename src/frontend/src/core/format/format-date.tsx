0232f263d8d2094e6aae9882fffda099536a5d01
kubrickcode <kubrickcode@gmail.com>
2024-12-29 22:41:50 +0000

    refactor: function return with keyword

--- src/frontend/src/core/format/format-date.tsx
+++ src/frontend/src/core/format/format-date.tsx
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
 
-export const formatDateTime = (date?: Date | null) =>
-  date ? dayjs(date).format("YYYY-MM-DD HH:mm") : "정보 없음";
+export const formatDateTime = (date?: Date | null) => {
+  return date ? dayjs(date).format("YYYY-MM-DD HH:mm") : "정보 없음";
+};
