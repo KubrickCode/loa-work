02b5bbfb5b73e2aff325458d910dd76db4139221
kubrickcode <kubrickcode@gmail.com>
2025-01-04 12:19:59 +0000

    푸터 오픈채팅 링크 추가

--- src/frontend/src/core/layout/footer/footer.tsx
+++ src/frontend/src/core/layout/footer/footer.tsx
@@ -3,8 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <Flex as="footer" justifyContent="center" p={2} mt="auto" width="100%">
+    <Flex
+      as="footer"
+      justifyContent="center"
+      gap={4}
+      p={2}
+      mt="auto"
+      width="100%"
+    >
       <Link to="/privacy-policy">개인정보 처리방침</Link>
+      <Link to={import.meta.env.VITE_KAKAO_OPEN_TALK_URL}>오픈채팅</Link>
     </Flex>
   );
 };
