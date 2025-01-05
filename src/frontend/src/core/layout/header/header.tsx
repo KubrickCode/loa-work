7e0285396ee0b9bbfa2184e44b6c457d60561d12
kubrickcode <kubrickcode@gmail.com>
2025-01-05 13:38:58 +0000

    메인 헤더에 링크 추가

--- src/frontend/src/core/layout/header/header.tsx
+++ src/frontend/src/core/layout/header/header.tsx
@@ -2,12 +2,15 @@ import { Flex, Heading } from "@chakra-ui/react";
 
 import { Navigation } from "../../navigation";
 import { HeaderMenu } from "./header-menu";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <Flex as="header" justifyContent="space-between" p={2}>
       <Flex alignItems="center" gap={4}>
-        <Heading display={{ base: "none", md: "block" }}>로직장</Heading>
+        <Link to="/">
+          <Heading display={{ base: "none", md: "block" }}>로직장</Heading>
+        </Link>
         <Navigation />
       </Flex>
       <HeaderMenu />
