fb83ef42183a675e7d8167e46a498762a3fc29a1
kubrickcode <kubrickcode@gmail.com>
2025-01-19 10:46:58 +0000

    stat 테이블에 item_id 인덱스 추가

---
+++ src/backend/prisma/migrations/20250119104629_create_index_item_id_to_stat/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "auction_item_stat_auction_item_id_idx" ON "auction_item_stat"("auction_item_id");
+
+-- CreateIndex
+CREATE INDEX "market_item_stat_market_item_id_idx" ON "market_item_stat"("market_item_id");
