2e8ce9e97cbea9c143eecf2024866ac032dc60c8
kubrickcode <kubrickcode@gmail.com>
2025-02-16 01:35:12 +0000

    controlled select 생성

---
+++ src/frontend/src/core/form/select.tsx
@@ -0,0 +1,38 @@
+import { Controller } from "react-hook-form";
+
+import {
+  Select as BaseSelect,
+  SelectProps as BaseSelectProps,
+} from "~/core/select";
+
+export type SelectProps<T extends string | number> = Omit<
+  BaseSelectProps<T>,
+  "onChange" | "value"
+> & {
+  items: { label: string; value: T }[];
+  name: string;
+};
+
+export const Select = <T extends string | number>({
+  items,
+  name,
+  placeholder,
+  ...props
+}: SelectProps<T>) => {
+  return (
+    <Controller
+      name={name}
+      render={({ field: { onChange, value } }) => {
+        return (
+          <BaseSelect
+            items={items}
+            onChange={onChange}
+            placeholder={placeholder}
+            value={value}
+            {...props}
+          />
+        );
+      }}
+    />
+  );
+};
