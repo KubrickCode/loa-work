2b0dd4245bd00a465aeb1f14c111707b864bb6b5
kubrickcode <kubrickcode@gmail.com>
2025-02-16 15:05:39 +0000

    테이블 row hover 효과 추가

--- src/frontend/src/core/table/data-table.tsx
+++ src/frontend/src/core/table/data-table.tsx
@@ -138,7 +138,7 @@ export const DataTable = <T,>({
 
   return (
     <Table.ScrollArea maxHeight="4xl">
-      <Table.Root showColumnBorder stickyHeader>
+      <Table.Root interactive showColumnBorder stickyHeader>
         <Table.Header>
           <Table.Row>
             {columns.map((column, index) => (
