c1d279cd758f4952bd3321d3859a43cc7c1c1753
kubrickcode <kubrickcode@gmail.com>
2025-02-21 12:28:48 +0000

    불필요한 privacy-policy 페이지 경로 환경변수 제거

--- src/frontend/src/core/layout/footer/footer.tsx
+++ src/frontend/src/core/layout/footer/footer.tsx
@@ -13,7 +13,7 @@ export const Footer = () => {
       p={2}
       width="100%"
     >
-      <Link href={import.meta.env.VITE_PRIVACY_POLICY_URL} target="_blank">
+      <Link href="/privacy-policy" target="_blank">
         개인정보처리방침
       </Link>
       <Link href={import.meta.env.VITE_KAKAO_OPEN_TALK_URL} target="_blank">
