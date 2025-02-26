96cfdb22fa194c7b74f2235cbb1346ca2c7c3176
kubrickcode <kubrickcode@gmail.com>
2025-02-26 14:25:36 +0000

    소유자가 컨텐츠 소요시간 수정 시 서버 데이터가 수정되도록 개선

---
+++ src/backend/prisma/migrations/20250226141700_add_column_is_edited_user_content_duration/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "user_content_duration" ADD COLUMN     "is_edited" BOOLEAN NOT NULL DEFAULT false;
--- src/backend/src/content/mutation/user-content-duration-edit.mutation.ts
+++ src/backend/src/content/mutation/user-content-duration-edit.mutation.ts
@@ -10,6 +10,9 @@ import {
 import { AuthGuard } from 'src/auth/auth.guard';
 import { PrismaService } from 'src/prisma';
 import { UserContentService } from '../../user/service/user-content.service';
+import { CurrentUser } from 'src/common/decorator/current-user.decorator';
+import { User } from 'src/common/object/user.object';
+import { Prisma, UserRole } from '@prisma/client';
 
 @InputType()
 class UserContentDurationEditInput {
@@ -37,16 +40,52 @@ export class UserContentDurationEditMutation {
   @Mutation(() => UserContentDurationEditResult)
   async userContentDurationEdit(
     @Args('input') input: UserContentDurationEditInput,
+    @CurrentUser() user: User,
   ) {
     const { id, value } = input;
 
     await this.userContentService.validateUserContentDuration(id);
 
-    await this.prisma.userContentDuration.update({
-      where: { id },
-      data: { value },
+    return await this.prisma.$transaction(async (tx) => {
+      if (user.role === UserRole.OWNER) {
+        await this.editDefaultContentDuration(input, tx);
+      }
+
+      await tx.userContentDuration.update({
+        where: { id },
+        data: { isEdited: true, value },
+      });
+
+      return { ok: true };
+    });
+  }
+
+  async editDefaultContentDuration(
+    { id, value }: UserContentDurationEditInput,
+    tx: Prisma.TransactionClient,
+  ) {
+    const { contentDurationId } =
+      await tx.userContentDuration.findUniqueOrThrow({
+        where: { id },
+      });
+
+    await tx.userContentDuration.updateMany({
+      where: {
+        contentDurationId,
+        isEdited: false,
+      },
+      data: {
+        value,
+      },
     });
 
-    return { ok: true };
+    await tx.contentDuration.update({
+      where: {
+        id: contentDurationId,
+      },
+      data: {
+        defaultValue: value,
+      },
+    });
   }
 }
