722706a7577be49d17b2efb8ad605bebb34ad75f
kubrickcode <kubrickcode@gmail.com>
2025-03-13 11:13:25 +0000

    누락된 테스트 코드 수정

--- src/backend/src/content/service/content-wage.service.spec.ts
+++ src/backend/src/content/service/content-wage.service.spec.ts
@@ -4,7 +4,6 @@ import { ContentWageService } from './content-wage.service';
 import { UserContentService } from '../../user/service/user-content.service';
 import { CONTEXT } from '@nestjs/graphql';
 import { UserGoldExchangeRateService } from 'src/user/service/user-gold-exchange-rate.service';
-import { ContentRewardItemService } from './content-reward-item.service';
 
 // 현재 중요 테스트는 해당 서비스에만 필요한 상황이라 아래와 같이 작성하고, 필요 시 팩토리 코드로 아래 데이터들을 생성하고 재사용할 수 있도록 해야함.
 describe('ContentWageService', () => {
@@ -18,7 +17,6 @@ describe('ContentWageService', () => {
         ContentWageService,
         UserContentService,
         UserGoldExchangeRateService,
-        ContentRewardItemService,
         {
           provide: CONTEXT,
           useValue: { req: { user: { id: undefined } } },
