9449b9faf276332e5f76dcb9e699f7d6486dd6f5
kubrickcode <kubrickcode@gmail.com>
2025-03-17 13:31:57 +0000

    test: validateUserGoldExchangeRate

--- src/backend/src/user/service/user-gold-exchange-rage.service.spec.ts
+++ src/backend/src/user/service/user-gold-exchange-rage.service.spec.ts
@@ -3,7 +3,7 @@ import { PrismaService } from 'src/prisma';
 import { CONTEXT } from '@nestjs/graphql';
 import { UserGoldExchangeRateService } from 'src/user/service/user-gold-exchange-rate.service';
 import { UserFactory } from 'src/test/factory/user.factory';
-import { User } from '@prisma/client';
+import { User, UserGoldExchangeRate } from '@prisma/client';
 
 describe('UserGoldExchangeRateService', () => {
   let module: TestingModule;
@@ -37,38 +37,49 @@ describe('UserGoldExchangeRateService', () => {
     const userGoldAmount = 100;
     const userKrwAmount = 30;
     let user: User;
+    let userGoldExchangeRate: UserGoldExchangeRate;
 
     beforeAll(async () => {
       user = await userFactory.create();
       service['context'].req.user = { id: user.id };
+
+      await prisma.goldExchangeRate.create({
+        data: {
+          goldAmount: 100,
+          krwAmount: 25,
+        },
+      });
+
+      userGoldExchangeRate = await prisma.userGoldExchangeRate.create({
+        data: {
+          userId: user.id,
+          goldAmount: userGoldAmount,
+          krwAmount: userKrwAmount,
+        },
+      });
     });
 
     describe('getGoldExchangeRate', () => {
-      beforeAll(async () => {
-        await prisma.goldExchangeRate.create({
-          data: {
-            goldAmount: 100,
-            krwAmount: 25,
-          },
-        });
-      });
-
       it('basic', async () => {
-        const user = await userFactory.create();
-        service['context'].req.user = { id: user.id };
-
-        await prisma.userGoldExchangeRate.create({
-          data: {
-            userId: user.id,
-            goldAmount: userGoldAmount,
-            krwAmount: userKrwAmount,
-          },
-        });
-
         const result = await service.getGoldExchangeRate();
 
         expect(result.goldAmount).toEqual(userGoldAmount);
         expect(result.krwAmount).toEqual(userKrwAmount);
+      });
+    });
+
+    describe('validateUserGoldExchangeRate', () => {
+      it('basic', async () => {
+        const result = await service.validateUserGoldExchangeRate(
+          userGoldExchangeRate.id,
+        );
+        expect(result).toEqual(true);
+      });
+
+      it('not matched', async () => {
+        await expect(
+          service.validateUserGoldExchangeRate(userGoldExchangeRate.id + 1),
+        ).rejects.toThrow('환율에 대한 수정 권한이 없습니다');
       });
     });
   });
