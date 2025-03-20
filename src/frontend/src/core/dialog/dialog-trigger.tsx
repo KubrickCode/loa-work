eca09c2d5f8da88565a81490bc507c875af1d5d2
kubrickcode <kubrickcode@gmail.com>
2025-03-20 11:53:01 +0000

    dialog 생성 로직 트리거 없이 사용하기 위해 훅 분리

--- src/frontend/src/core/dialog/dialog-trigger.tsx
+++ src/frontend/src/core/dialog/dialog-trigger.tsx
@@ -1,17 +1,10 @@
-import { Center, Flex, FlexProps, useDisclosure } from "@chakra-ui/react";
-import {
-  ComponentPropsWithoutRef,
-  createElement,
-  ElementType,
-  Suspense,
-} from "react";
+import { Flex, FlexProps } from "@chakra-ui/react";
+import { ElementType } from "react";
 
-import { BlockLoader } from "../loader";
+import { useDialog, UseDialogProps } from "./use-dialog";
 
-export type DialogTriggerProps<T extends ElementType> = FlexProps & {
-  dialog: T;
-  dialogProps?: Omit<ComponentPropsWithoutRef<T>, "open" | "onClose">;
-};
+export type DialogTriggerProps<T extends ElementType> = FlexProps &
+  UseDialogProps<T>;
 
 export const DialogTrigger = <T extends ElementType>({
   children,
@@ -19,21 +12,7 @@ export const DialogTrigger = <T extends ElementType>({
   dialogProps,
   ...otherProps
 }: DialogTriggerProps<T>) => {
-  const { onOpen, onClose, open } = useDisclosure();
-
-  const renderModal = () => {
-    if (!open) return null;
-
-    return (
-      <Suspense fallback={<BackdropLoader />}>
-        {createElement(dialog, {
-          ...dialogProps,
-          onClose,
-          open,
-        })}
-      </Suspense>
-    );
-  };
+  const { onOpen, renderModal } = useDialog({ dialog, dialogProps });
 
   return (
     <>
@@ -48,21 +27,5 @@ export const DialogTrigger = <T extends ElementType>({
       </Flex>
       {renderModal()}
     </>
-  );
-};
-
-const BackdropLoader = () => {
-  return (
-    <Center
-      bg="blackAlpha.600"
-      bottom={0}
-      left={0}
-      position="fixed"
-      right={0}
-      top={0}
-      zIndex="modal"
-    >
-      <BlockLoader />
-    </Center>
   );
 };
---
+++ src/frontend/src/core/dialog/use-dialog.tsx
@@ -0,0 +1,59 @@
+import { Center, useDisclosure } from "@chakra-ui/react";
+import {
+  ComponentPropsWithoutRef,
+  createElement,
+  ElementType,
+  Suspense,
+} from "react";
+
+import { BlockLoader } from "../loader";
+
+export type UseDialogProps<T extends ElementType> = {
+  dialog: T;
+  dialogProps?: Omit<ComponentPropsWithoutRef<T>, "open" | "onClose">;
+};
+
+export const useDialog = ({
+  dialog,
+  dialogProps,
+}: UseDialogProps<ElementType>) => {
+  const { onOpen, onClose, open, onToggle } = useDisclosure();
+
+  const renderModal = () => {
+    if (!open) return null;
+
+    return (
+      <Suspense fallback={<BackdropLoader />}>
+        {createElement(dialog, {
+          ...dialogProps,
+          onClose,
+          open,
+        })}
+      </Suspense>
+    );
+  };
+
+  return {
+    onClose,
+    onOpen,
+    onToggle,
+    open,
+    renderModal,
+  };
+};
+
+const BackdropLoader = () => {
+  return (
+    <Center
+      bg="blackAlpha.600"
+      bottom={0}
+      left={0}
+      position="fixed"
+      right={0}
+      top={0}
+      zIndex="modal"
+    >
+      <BlockLoader />
+    </Center>
+  );
+};
