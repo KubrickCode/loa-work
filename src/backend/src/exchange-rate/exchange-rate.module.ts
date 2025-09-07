1823ae844ddd5af693c827d0691aac104dd2acae
kubrickcode <kubrickcode@gmail.com>
2025-09-07 08:53:16 +0000

    파일명 오타 수정
    rage -> rate

--- src/backend/src/exchange-rate/exchange-rate.module.ts
+++ src/backend/src/exchange-rate/exchange-rate.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma';
-import { GoldExchangeRateQuery } from './query/gold-exchange-rage.query';
+import { GoldExchangeRateQuery } from './query/gold-exchange-rate.query';
 import { GoldExchangeRateEditMutation } from './mutation/gold-exchange-rate-edit.mutation';
 import { UserGoldExchangeRateService } from 'src/user/service/user-gold-exchange-rate.service';
 import { DiscordModule } from 'src/discord/discord.module';
