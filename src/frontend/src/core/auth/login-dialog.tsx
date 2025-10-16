18161ab8072fb6106210241543dbebeccd434e26
kubrickcode <kubrickcode@gmail.com>
2025-10-16 07:46:38 +0000

    로그인 다이얼로그가 예상치 못한 동작과 함께 열리는 문제 수정
    DialogContent 는 현재 공용 Dialog 내에서 렌더링 되는 것으로 최근 수정되었으나, 해당 수정사항 반영이 로그인 다이얼로그에서 누락되어 수정함(사용처에서 DialogContent 제거해야 함).
    
    fix #166

--- src/frontend/src/core/auth/login-dialog.tsx
+++ src/frontend/src/core/auth/login-dialog.tsx
@@ -9,46 +9,44 @@ import { Dialog, DialogProps } from "~/core/dialog";
 export const LoginDialog = (dialogProps: DialogProps) => {
   return (
     <Dialog {...dialogProps}>
-      <Dialog.Content>
-        <Dialog.Header>로그인</Dialog.Header>
-        <Dialog.Body>
-          <Flex direction="column" gap={3}>
-            <Button
-              _hover={{ bg: "gray.50" }}
-              bg="white"
-              borderColor="gray.200"
-              borderWidth={1}
-              color="gray.700"
-              onClick={() => {
-                window.location.href = "/auth/google";
-              }}
-            >
-              <FcGoogle /> Google로 계속하기
-            </Button>
-            <Button
-              _hover={{ bg: "#4752C4" }}
-              bg="#5865F2"
-              color="white"
-              onClick={() => {
-                window.location.href = import.meta.env.VITE_DISCORD_LOGIN_URL;
-              }}
-            >
-              <FaDiscord /> Discord로 계속하기
-            </Button>
-            <Button
-              _hover={{ bg: "#FDD835" }}
-              bg="#FEE500"
-              color="rgba(0,0,0,0.85)"
-              onClick={() => {
-                window.location.href = "/auth/kakao";
-              }}
-            >
-              <RiKakaoTalkFill /> 카카오로 계속하기
-            </Button>
-          </Flex>
-        </Dialog.Body>
-        <Dialog.Footer />
-      </Dialog.Content>
+      <Dialog.Header>로그인</Dialog.Header>
+      <Dialog.Body>
+        <Flex direction="column" gap={3}>
+          <Button
+            _hover={{ bg: "gray.50" }}
+            bg="white"
+            borderColor="gray.200"
+            borderWidth={1}
+            color="gray.700"
+            onClick={() => {
+              window.location.href = "/auth/google";
+            }}
+          >
+            <FcGoogle /> Google로 계속하기
+          </Button>
+          <Button
+            _hover={{ bg: "#4752C4" }}
+            bg="#5865F2"
+            color="white"
+            onClick={() => {
+              window.location.href = import.meta.env.VITE_DISCORD_LOGIN_URL;
+            }}
+          >
+            <FaDiscord /> Discord로 계속하기
+          </Button>
+          <Button
+            _hover={{ bg: "#FDD835" }}
+            bg="#FEE500"
+            color="rgba(0,0,0,0.85)"
+            onClick={() => {
+              window.location.href = "/auth/kakao";
+            }}
+          >
+            <RiKakaoTalkFill /> 카카오로 계속하기
+          </Button>
+        </Flex>
+      </Dialog.Body>
+      <Dialog.Footer />
     </Dialog>
   );
 };
