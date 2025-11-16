7a19a8d3e26241ed669ed6a7a7f67e5bad2fcce3
kubrickcode <kubrickcode@gmail.com>
2025-11-16 10:18:23 +0000

    chore: generated.tsx 파일이 husky lint-staged 스킵하도록 수정

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "src/backend/**/*.ts": () => "just lint backend",
-  "src/frontend/**/*.{ts,tsx}": () => "just lint frontend",
+  "src/frontend/**/!(generated).{ts,tsx}": () => "just lint frontend",
   "src/go/**/*.go": () => "just lint go",
   "**/*.{json,yml,yaml,md}": () => "just lint config",
   justfile: () => "just lint justfile",
